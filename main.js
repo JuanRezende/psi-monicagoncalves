741be7e258d6eb6e27df8ada220a6b8428d5f5ab
Juan Rezende <juanvictor.rezende11@gmail.com>
2024-03-20 19:33:12 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+window.addEventListener("scroll", onScroll);
+onScroll();
+
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopOnScroll();
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  const sectionEndAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndAt <= targetLine;
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    document.querySelector("#navigation").classList.add("scroll");
+    document.querySelector(".logo img").src = "/PSIMonica/assets/MonicaGoncalvesV2.png";
+  } else {
+    document.querySelector("#navigation").classList.remove("scroll");
+    document.querySelector(".logo img").src = "/PSIMonica/assets/MonicaGoncalvesV1.png";
+  }
+}
+
+function showBackToTopOnScroll() {
+  if (scrollY > 550) {
+    document.querySelector("#backToTopButton").classList.add("show");
+  } else {
+    document.querySelector("#backToTopButton").classList.remove("show");
+  }
+}
+
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
+
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(`
+    #home, 
+    #home img,
+    #home .stats, 
+    #services,
+    #services header,
+    #services .card
+    #about,
+    #about .header
+    #about .content`);
